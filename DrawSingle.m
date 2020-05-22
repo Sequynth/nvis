0f9142a9c4e9783d6d3f8228a3d67a3d8f61886e
Johannes Fischer <johannes.fischer@uniklinik-freiburg.de>
2020-05-22 12:24:17 +0200

    default initial position and size set to leave a margin of 10% in y-direction to total screensize

--- DrawSingle.m
+++ DrawSingle.m
@@ -148,8 +148,6 @@ classdef DrawSingle < Draw
     
     properties (Constant, Access = private)
         % UI PROPERTIES
-        % default figure position and size
-        defaultPosition = [ 300, 50, 800, 700];
         % absolute width of Control panel in pixel
         controlWidth  = 300; % px        
         sliderHeight  = 20;  % px 
@@ -189,11 +187,16 @@ classdef DrawSingle < Draw
                 obj.standardTitle = inputname(1);
             end
             
+            % default figure position and size. is adapdet to actual screensize
+            % and is separated from top/bottom by 10% of up/down screensize
+            screenS = get(0, 'ScreenSize');
+            defaultPosition = [ 300, round(0.1*screenS(4)), 800, round(0.8*screenS(4))];
+            
             obj.prepareParser()
             
             % additional parameters
             addParameter(obj.p, 'InitRot',          0,                                  @(x) isnumeric(x));
-            addParameter(obj.p, 'Position',         obj.defaultPosition,                @(x) isnumeric(x) && numel(x) == 4);
+            addParameter(obj.p, 'Position',         defaultPosition,                    @(x) isnumeric(x) && numel(x) == 4);
             addParameter(obj.p, 'InitSlice',        round(obj.S(obj.mapSliderToDim)/2), @isnumeric);
             addParameter(obj.p, 'fps',              0,                                  @isnumeric);
             addParameter(obj.p, 'ROI_Signal',       [0 0; 0 0; 0 0],                    @isnumeric);
